f8cd312a165805af3e18a0aef70dfdc33ff71c32
David Soden <36846872+dtsoden@users.noreply.github.com>
2025-01-18 11:36:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -142,4 +142,4 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Create database tables
     #app.run(debug=True)
-    app.run('0.0.0.0', port=88)
+    app.run('0.0.0.0', port=80)
