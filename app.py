e69bb4b05043fe237355b0009e16ea87488ab598
David Soden <36846872+dtsoden@users.noreply.github.com>
2025-01-18 11:27:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -141,4 +141,5 @@ def delete_link(unique_id):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Create database tables
-    app.run(debug=True)
+    #app.run(debug=True)
+    app.run('0.0.0.0', port=88)
